3e733f655788c98cbe378c38fef2b2b24426c52a
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-12-11 13:25:50 +0100

    update

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
 window.addEventListener('load', () => {
     const homeNav = document.getElementById('homeNav');
+    const trailerNav = document.getElementById('trailerNav');
     const lineUpNav = document.getElementById('lineUpNav');
     const ticketsNav = document.getElementById('ticketsNav');
+    const merchNav = document.getElementById('merchNav');
     const impressionsNav = document.getElementById('impressionsNav');
+    const trailer = document.getElementById('trailer');
+    let lastscrolly = 0;
+    let ticking = false;
 
     homeNav.addEventListener('click', () => {
         window.scrollTo({
@@ -12,7 +17,7 @@ window.addEventListener('load', () => {
         });
     });
 
-    lineUpNav.addEventListener('click', () => {
+    trailerNav.addEventListener('click', () => {
         window.scrollTo({
             top: document.getElementById('homeSection').clientHeight + 1,
             left: 0,
@@ -20,9 +25,25 @@ window.addEventListener('load', () => {
         });
     });
 
+    lineUpNav.addEventListener('click', () => {
+        window.scrollTo({
+            top: document.getElementById('homeSection').clientHeight + document.getElementById('trailerSection').clientHeight + 1,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+
     ticketsNav.addEventListener('click', () => {
         window.scrollTo({
-            top: document.getElementById('homeSection').clientHeight + document.getElementById('lineUpSection').clientHeight + 1,
+            top: document.getElementById('homeSection').clientHeight + document.getElementById('lineUpSection').clientHeight + document.getElementById('trailerSection').clientHeight + 1,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    merchNav.addEventListener('click', () => {
+        window.scrollTo({
+            top: document.getElementById('homeSection').clientHeight + document.getElementById('lineUpSection').clientHeight + document.getElementById('ticketSection').clientHeight + document.getElementById('trailerSection').clientHeight + 1,
             left: 0,
             behavior: 'smooth'
         });
@@ -30,9 +51,27 @@ window.addEventListener('load', () => {
 
     impressionsNav.addEventListener('click', () => {
         window.scrollTo({
-            top: document.getElementById('homeSection').clientHeight + document.getElementById('lineUpSection').clientHeight + document.getElementById('ticketSection').clientHeight + 1,
+            top: document.getElementById('homeSection').clientHeight + document.getElementById('lineUpSection').clientHeight + document.getElementById('ticketSection').clientHeight + document.getElementById('merchSection').clientHeight + document.getElementById('trailerSection').clientHeight + 1,
             left: 0,
             behavior: 'smooth'
         });
+    });
+
+    window.addEventListener('scroll', (ev) => {
+    lastscrolly = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            if (document.getElementById('homeSection').clientHeight / 2 + 1 < lastscrolly && document.getElementById('homeSection').clientHeight + document.getElementById('trailerSection').clientHeight / 2 + 1 > lastscrolly) {
+                trailer.play();
+            } else {
+                trailer.pause();
+            }
+
+            ticking = false;
+        });
+
+        ticking = true;
+    }
     });
 });
